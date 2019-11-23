97f65516c8a53bae420a2e6f2a26cef2c45ca78d
Nicolás Aimetti <naimetti@yahoo.com.ar>
2019-11-23 14:23:45 -0300

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ansible_template_validator",
-    version="0.1.0",
+    version="0.1.1",
     author="Nicolas Aimetti",
     author_email="naimetti@yahoo.com.ar",
     description="A helper script to use with the validate option from ansible template module",
